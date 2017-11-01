abead5930fe8c4e08e6ec8de203428cb81ceac44
Joost <joost@meijles.com>
2017-11-01 22:03:22 +0100

    Introduct sub-commands

---
+++ cli.js
@@ -0,0 +1,42 @@
+// Parse command-line options
+const ArgumentParser = require('argparse').ArgumentParser;
+
+const parser = new ArgumentParser({
+    version: require('./package.json').version,
+    addHelp: true,
+    description: 'Get JIRA ticket information from the Git log of the currently active branch, starting from tag until to tag.'
+});
+
+const subparsers = parser.addSubparsers({
+    dest: 'subcommand_name'
+});
+
+// ls sub-command
+const ls = subparsers.addParser('list', {
+    addHelp: true,
+    aliases: ['ls'],
+    description: 'List all JIRA tickets'
+});
+ls.addArgument('from', {help: 'Git tag to start from (excluded)'});
+ls.addArgument('to', {help: 'Git tag to end with (included)'});
+ls.addArgument(
+    '--jira-config', 
+    {help: 'JSON file containing Jira connection details and authentication credentials, see: jira_example_config.json and https://www.npmjs.com/package/jira-connector'}
+);
+ls.setDefaults();
+
+const check = subparsers.addParser('verify', {
+    addHelp: true,
+    description: 'Verify if release information matches with JIRA administration'
+});
+check.addArgument('from', {help: 'Git tag to start from (excluded)'});
+check.addArgument('to', {help: 'Git tag to end with (included)'});
+check.addArgument(
+    '--jira-config', 
+    {
+        help: 'JSON file containing Jira connection details and authentication credentials, see: jira_example_config.json and https://www.npmjs.com/package/jira-connector',
+        required: true
+    }
+);
+
+module.exports = parser;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const ArgumentParser = require('argparse').ArgumentParser;
+const cli = require('./cli');
 const git = require('./git');
 const jira = require('./jira');
 const path = require("path");
@@ -8,6 +8,9 @@ function printIssueLine(keyIssue) {
     const {key, issue} = keyIssue;
     if (issue) {
         console.log(`${key} ${issue.fields.summary}`);
+
+        const fixVersions = issue.fields.fixVersions;
+        const releases = fixVersions.map(v => v.name).join(' ');
     } else {
         console.log(`${key}`);
     }
@@ -29,24 +32,31 @@ function handleLog(log, jiraClient) {
     }
 }
 
-const parser = new ArgumentParser({
-    version: require('./package.json').version,
-    addHelp: true,
-    description: 'Get JIRA tickets from the Git log of the currently active branch, starting from tag until to tag.'
-});
+function list(args) {
+    let jiraClient = null;
+    if (args.jira_config) {
+        const configFile = path.resolve(args.jira_config);
+        jiraClient = jira.createClient(configFile);
+    }
 
-parser.addArgument('from', {help: 'Git tag to start from (not included)'});
-parser.addArgument('to', {help: 'Git tag to end with (included)'});
-parser.addArgument(
-    '--jira-config', 
-    {help: 'JSON file containing Jira connection details and authentication credentials, see: jira_example_config.json and https://www.npmjs.com/package/jira-connector'});
-const args = parser.parseArgs();
-
-let jiraClient = null;
-if (args.jira_config) {
-    const configFile = path.resolve(args.jira_config);
-    jiraClient = jira.createClient(configFile);
+    const options = {from: args.from, to: args.to};
+    git.getLog(options, (log) => handleLog(log, jiraClient));
 }
 
-const options = {from: args.from, to: args.to};
-git.getLog(options, (log) => handleLog(log, jiraClient));
+function verify(args) {
+    console.log('Not implemented');
+}
+
+function main() {
+    const args = cli.parseArgs();
+
+    switch(args.subcommand_name) {
+        case 'list':
+        case 'ls':
+            return list(args);
+        case 'verify':
+            return verify(args);
+    }
+}
+
+main();
